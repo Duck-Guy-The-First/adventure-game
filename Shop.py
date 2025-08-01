a13207a281ef269ced5108f6ec58a5000699c9cb
Duck-Guy-The_First <rfarmahin2013@gmail.com>
2025-08-01 10:26:19 -0400

    Shop

--- Shop.py
+++ Shop.py
@@ -1,7 +1,7 @@
 def shop(coin,inventory):
     print("HELLO")
     while(True):
-        ChooseShop=int(input("WHAT DO THY WANT FROM MY SHOP DO THY WANT TO CHECK OR BUY MY ITEMS\n[1] Check\n[2] Buy\n[3] Leave\n[4] Steal\n"))
+        ChooseShop=int(input("WHAT DO THY WANT FROM MY SHOP\n[1] Check\n[2] Buy\n[3] Leave\n"))
         if(ChooseShop==1):
             while(True):
                 CheckOption=input("DO THY WANT TO CHECK MY [1] HEALTH POTION\nOR DO THY WANT TO CHECK MY [2] Broken chat filter\nOR DO THY WANT TO CHECK MY [3] KILLING POTION\nOR DO THY WANT TO GO BACK [B]\n")
@@ -13,11 +13,33 @@ def shop(coin,inventory):
                     print("THIS POTION TURNS THY IN TO A KILLING MACHINE IT GIVES THY 15% MORE DAMAGE IN A BATTLE FOR 3 TURNS. I WOULD 100% RECOMMEND IT.")
                 if(CheckOption=="B"):
                     break
-    
+            # all coins for Walmart Shopping Bag
         elif(ChooseShop==2):
             while(True):
-                BuyOption=int(input("DO THY WANT [1] HEALTH POTION - 6 coins\nOR DO THY WANT MY [2] Broken chat filter - 7 coins\n OR DO THY WANT MY [3] KILLING POTION - 15 coins\nOR DO THY WANT MY [4] Walmart Shopping Bag - 1000"))
-            # all coins for Walmart Shopping Bag
+                print("Coins: ",coin)
+                BuyOption=input("DO THY WANT [1] HEALTH POTION - 6 coins\nOR DO THY WANT MY [2] Broken chat filter - 8 coins\n OR DO THY WANT MY [3] KILLING POTION - 15 coins\nOR DO THY WANT MY [4] Walmart Shopping Bag - 1000 coins\n[B] Go back\n")
+                if(BuyOption=="1" and coin>=6):
+                    print("You got a health potion")
+                    coin-=6
+                else:
+                    print("IT SEEMS THAT THY DON'T A ENOUGH FOR A HEALTH POTION")
+                if(BuyOption=="2" and coin>=8):
+                    print("You got the broken chat filter")
+                    coin-=8
+                else:
+                    print("IT SEEMS THAT YOU DON'T A ENOUGH FOR THE BROKEN CHAT FILTER")
+                if(BuyOption=="3" and coin>=15):
+                    print("You got a killing potion")
+                    coin-=15
+                else:
+                    print("IT SEEMS THAT YOU DON'T A ENOUGH FOR A KILLING POTION")
+                if(BuyOption=="4" and coin>=1000):
+                    coin-=1000
+                    print("HOW DID THY BUY THE WALMART SHOPING BAG?")
+                else:
+                    print("IT'S THE MOST EXPENSIVE ITEM WHY DO YOU THING YOU CAN'T BUY IT")
+                if(BuyOption=="B"):
+                    break
         elif(ChooseShop==3):
             break
 shop(coin=25,inventory=25)
