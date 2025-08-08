181466a846e1885cfd3930608eaa55f95d129523
laveshnk-crypto <laveshnk@gmail.com>
2025-08-08 14:49:15 -0400

    test

--- Test 1.py
+++ Test 1.py
@@ -1,5 +1,14 @@
-num1 = 0
+# num1 = 0
 
-while(num1 <=10):
-    print("Ryan is a plumber with a purple hat (Wario)")
-    num1 += 1
+# while(num1 <=10):
+#     print("Ryan is a plumber with a purple hat (Wario)")
+#     num1 += 1
+
+fruits = ["Mango", "Jupiter", "Ryan", "Tara", "The hit music video: Everywhere the end of time"]
+
+# print out: I like eating _____
+
+for fruit in fruits:
+    print("I like eating", fruit)
+
+# number * number
